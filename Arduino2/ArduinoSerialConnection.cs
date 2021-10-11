7306f8734956967a11849482a48aa5f29b21754e
billchetch <45006134+billchetch@users.noreply.github.com>
2021-10-11 23:00:34 +0800

    s

--- Arduino2/ArduinoSerialConnection.cs
+++ Arduino2/ArduinoSerialConnection.cs
@@ -11,6 +11,7 @@ namespace Chetch.Arduino2
     {
         public const String BOARD_CH340 = "CH340";
         public const String BOARD_UNO = "Arduino Uno";
+        public const String BOARD_MEGA = "Arduino Mega";
 
         public ArduinoSerialConnection(String portName, int baudRate) : base(portName, baudRate)
         {
